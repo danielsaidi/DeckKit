c98cce2497fabe349837bec515f3cc1e10ea9665
Daniel Saidi <daniel.saidi@gmail.com>
2022-04-29 15:36:00 +0200

    Add DocC plugin dependency

--- Package.swift
+++ Package.swift
@@ -4,13 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "DeckKit",
-    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "DeckKit",
             targets: ["DeckKit"]),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "DeckKit",
