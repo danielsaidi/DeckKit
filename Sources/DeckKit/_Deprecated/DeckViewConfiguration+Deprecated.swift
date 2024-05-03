df28e408c114fb93fd2f2791c6f0ce70d024d8b8
Daniel Saidi <daniel.saidi@gmail.com>
2024-05-03 23:19:24 +0200

    Remove deprecated code

--- Sources/DeckKit/_Deprecated/DeckViewConfiguration+Deprecated.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-#if os(iOS) || os(macOS)
-public extension DeckViewConfiguration {
-
-    @available(*, deprecated, message: "This value will be removed in the next major update. Just create a custom configuration when you need to.")
-    static var down: Self { .init(direction: .down) }
-}
-#endif
