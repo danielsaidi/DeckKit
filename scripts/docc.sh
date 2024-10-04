92ac8bffe749704d8501d5954ab7202c40f68234
Daniel Saidi <daniel.saidi@gmail.com>
2024-10-04 08:34:33 +0200

    Adjust DocC script

--- scripts/docc.sh
+++ scripts/docc.sh
@@ -12,8 +12,8 @@ swift package resolve;
 xcodebuild docbuild -scheme $1 -derivedDataPath /tmp/docbuild -destination 'generic/platform=iOS';
 
 $(xcrun --find docc) process-archive \
-transform-for-static-hosting /tmp/docbuild/Build/Products/Debug-iphoneos/$1.doccarchive \
---output-path .build/docs \
---hosting-base-path '$1';
+  transform-for-static-hosting /tmp/docbuild/Build/Products/Debug-iphoneos/$1.doccarchive \
+  --output-path .build/docs \
+  --hosting-base-path '$1';
 
 echo "<script>window.location.href += \"/documentation/$TARGET_LOWERCASED\"</script>" > .build/docs/index.html;
