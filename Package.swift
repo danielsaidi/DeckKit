07e96e7c66f0cc885aa707318d74e8da35f2e8bf
Daniel Saidi <daniel.saidi@gmail.com>
2026-06-05 11:12:23 +0200

    Bump package to iOS 17

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "DeckKit",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8),
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17),
+        .watchOS(.v10),
         .visionOS(.v1)
     ],
     products: [
